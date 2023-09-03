823b4f0f6cf74d7a1e2df48e9e07b31ee346df45
enriquetapiaan <tapiaa.antunes@outlook.com>
2023-09-03 12:10:29 -0600

    agrege los estilos css y la maquetación inicial

--- views/index.view.php
+++ views/index.view.php
@@ -3,11 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
     <title>Paginación</title>
 </head>
 <body>
     <div class="contenedor">
-        
+        <h1>Articulos</h1>
+        <section class="articulos">
+            <ul>
+                <li>1.- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore illo sus</li>
+                <li>2.- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore illo sus</li>
+                <li>3.- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore illo sus</li>
+                <li>4.- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore illo sus</li>
+                <li>5.- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore illo sus</li>
+                <li>6.- Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore illo sus</li>
+            </ul>
+        </section>
+        <section class="paginacion">
+            <ul>
+                <li><a href="#" class="disabled">&laquo;</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">&raquo;</a></li>
+            </ul>
+        </section>
     </div>
 </body>
 </html>
